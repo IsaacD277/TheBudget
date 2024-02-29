231aca0cf8be4ba21a6efc0ba032613539597864
isaacd2 <idarr08@gmail.com>
2024-02-29 14:24:46 -0500

    Calculate turned into an arrow and investment task was added.

--- TheBudget/TitheTask.swift
+++ TheBudget/TitheTask.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct TitheTask: View {
     @State private var income: String?
     @State private var donation: Double = 0.0
+    @State private var investment: Double = 0.0
     @State private var isDonated: Bool = true
+    @State private var isInvested: Bool = true
     @State private var focusIncome: Bool = true
     
     var body: some View {
@@ -23,7 +25,32 @@ struct TitheTask: View {
                         Button(action: {
                             isDonated = true
                             donation = 0.0
-                            focusIncome = true
+                            if isDonated && isInvested {
+                                focusIncome = true
+                            }
+                        }) {
+                            Image(systemName: "xmark")
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .padding()
+                                .background(Color.blue)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
+                    }
+                }
+                 
+                if !isInvested {
+                    HStack {
+                        Text("Invest: \(investment, format: .currency(code: "USD"))")
+                            .font(.title)
+                        Button(action: {
+                            isInvested = true
+                            investment = 0.0
+                            if isDonated && isInvested {
+                                focusIncome = true
+                            }
+                            
                         }) {
                             Image(systemName: "xmark")
                                 .foregroundColor(.white)
@@ -40,29 +67,38 @@ struct TitheTask: View {
                 
                 VStack {
                     VStack {
-                        Text("$\(income ?? "0")")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .foregroundColor(.blue)
-                            .opacity(focusIncome ? 1.0 : 0.3)
-                            .padding()
-                            .onTapGesture {
-                                focusIncome = true
+                        ZStack {
+                            Text("$\(income ?? "0")")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                                .foregroundColor(.blue)
+                                .opacity(focusIncome ? 1.0 : 0.3)
+                                .padding()
+                                .onTapGesture {
+                                    focusIncome = true
+                                }
+                            
+                            HStack {
+                                Spacer()
+                                
+                                Image(systemName: "arrowshape.right")
+                                    .foregroundColor(.white)
+                                    .fontWeight(.black)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                    .padding(.trailing)
+                                    .onTapGesture {
+                                        donation += (Double(income ?? "0.00") ?? 0.00) * 0.1
+                                        investment += (Double(income ?? "0.00") ?? 0.00) * 0.05
+                                        isDonated = false
+                                        isInvested = false
+                                        focusIncome = false
+                                        income = nil
+                                    }
+                                    .opacity(focusIncome ? 1.0 : 0.0)
                             }
-                        
-                        #warning("add an enter and an add button so that multiple incomes can be added at once.")
-                        Text("Calculate")
-                            .foregroundColor(.white)
-                            .fontWeight(.bold)
-                            .padding()
-                            .background(Color.blue)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .onTapGesture {
-                                donation += (Double(income ?? "0.00") ?? 0.00) * 0.1
-                                isDonated = false
-                                focusIncome = false
-                                // income = nil
-                            }
+                        }
                     }
                     .padding([.horizontal, .top])
                     .offset(y: focusIncome ? 0 : geometry.size.height / 2) // Slide down and out when not in focus
@@ -76,7 +112,7 @@ struct TitheTask: View {
                     .animation(.bouncy, value: focusIncome)
                 }
             }
-            .padding(.bottom, geometry.safeAreaInsets.bottom)
+            .padding(.bottom)
         }
     }
 }
