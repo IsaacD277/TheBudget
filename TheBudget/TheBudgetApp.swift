2046d0fc1d934cbd1fbb2e997ca0884fe12f27cc
isaacd2 <idarr08@gmail.com>
2024-02-28 20:11:22 -0500

    Initial Commit

---
+++ TheBudget/TheBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBudgetApp.swift
+//  TheBudget
+//
+//  Created by Isaac D2 on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TheBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
