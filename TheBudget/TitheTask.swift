400e690522576b9f06330cbea2c2590a7b8d636d
isaacd2 <idarr08@gmail.com>
2024-03-07 11:10:54 -0500

    Made the keyboard recognized by the system

--- TheBudget/TitheTask.swift
+++ TheBudget/TitheTask.swift
@@ -8,87 +8,74 @@
 import SwiftUI
 
 struct TitheTask: View {
-    @State private var income: String?
+    @State private var income: String = ""
     @State private var donation: Double = 0.0
     @State private var investment: Double = 0.0
-    @State private var focusIncome: Bool = true
-    
-    // Apple Monthly Expense Tracking
-    @State private var appleName: String = "Apple One"
-    @State private var appleDueDate: Int = 2
-    @State private var appleMonthlyBill: Double = 6.93
-    @State private var appleCurrentAmount: Double = 10.0
+    @FocusState private var focusIncome: Bool
     
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                ScrollView {
-                    LazyVStack {
-                        DonationConstant(donation: $donation)
-                            .padding(.horizontal)
-                        
-                        InvestmentConstant(investment: $investment)
-                            .padding(.horizontal)
-                        
-                        AppleOneConstant(Name: "Apple One")
-                            .padding(.horizontal)
-                        
-                        AppleOneConstant(Name: "Spotify")
-                            .padding(.horizontal)
-                    }
-                }
+        VStack {
+            ScrollView {
+                LazyVStack {
+                    AppleOneConstant(Name: "Apple One")
+                        .padding(.horizontal)
                     
-                Spacer()
-                
-                // KEYBOARD AND INPUT
-                VStack {
-                    // INPUT
-                    ZStack {
-                        Text("$\(income ?? "0")")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .foregroundColor(.blue)
-                            .opacity(focusIncome ? 1.0 : 0.3)
-                            .padding()
-                            .onTapGesture {
-                                focusIncome = true
-                            }
-                        
-                        HStack {
-                            Spacer()
-                            
-                            Image(systemName: "arrowshape.right")
-                                .foregroundColor(.white)
-                                .fontWeight(.black)
-                                .padding()
-                                .background(Color.blue)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .padding(.trailing)
-                                .onTapGesture {
-                                    donation += (Double(income ?? "0.00") ?? 0.00) * 0.1
-                                    investment += (Double(income ?? "0.00") ?? 0.00) * 0.05
-                                    focusIncome = false
-                                    income = nil
-                                }
-                                .opacity(focusIncome ? 1.0 : 0.0)
-                        }
-                    }
+                    AppleOneConstant(Name: "Spotify")
+                        .padding(.horizontal)
+                    
+                    DonationConstant(donation: $donation)
+                        .padding(.horizontal)
+                    
+                    InvestmentConstant(investment: $investment)
+                        .padding(.horizontal)
+                    
+                    // IF income ends up being optional
+//                        TextField("Paycheck Amount", text: Binding(
+//                            get: { income ?? "" },
+//                            set: { income = $0 }
+//                        ))
+                    
                 }
-                .padding([.horizontal, .top])
-                .offset(y: focusIncome ? 0 : geometry.size.height / 2) // Slide down and out when not in focus
-                .animation(.bouncy, value: focusIncome)
-                
-                CustomNumberKeyboard(inputText: Binding(
-                    get: { income ?? "" },
-                    set: { income = $0 }
-                ))
-                .offset(y: focusIncome ? 0 : geometry.size.height / 1.8) // Slide down and out when not in focus
-                .animation(.bouncy, value: focusIncome)
             }
-            .padding(.bottom)
+            .scrollDismissesKeyboard(.interactively)
+            .scrollIndicators(.never)
+            
+            Spacer()
+            
+            ZStack {
+                TextField("Amount", text: $income)
+                    .inputView {
+                        CustomNumberKeyboard(inputText: $income)
+                    }
+                    .multilineTextAlignment(.center)
+                    .focused($focusIncome)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.blue)
+                    .padding(.horizontal)
+                
+                HStack {
+                    Spacer()
+                    
+                    Image(systemName: "arrowshape.right")
+                        .foregroundColor(.white)
+                        .fontWeight(.black)
+                        .padding()
+                        .background(Color.blue)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding(.trailing)
+                        .onTapGesture {
+                            donation += (Double(income) ?? 0.00) * 0.1
+                            investment += (Double(income) ?? 0.00) * 0.05
+                            focusIncome = false
+                            income = ""
+                        }
+                }
+            }
         }
     }
 }
+                    
 
 #Preview {
     TitheTask()
